696cb0096850ebffcf3d6f82d24bf6b1cb753340
Oliver Stolz <oliver.stolz@iml.fraunhofer.de>
2024-04-29 15:44:21 +0200

    Handle error when canvas is undefined

--- client/src/canvas/draw_legend.ts
+++ client/src/canvas/draw_legend.ts
@@ -55,98 +55,100 @@ function drawRoundedRect(
 
 // set the canvas properties
 const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
-const width = window.innerWidth;
-canvas.width = width;
-const scalingFactor = width / 400; // adjust the legend size according to the screen width
-canvas.height = 460 * scalingFactor;
-const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+if (canvas != null) {
+  const width = window.innerWidth;
+  canvas.width = width;
+  const scalingFactor = width / 400; // adjust the legend size according to the screen width
+  canvas.height = 460 * scalingFactor;
+  const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
 
-// style properties for the legend that are found to fit well
-//// circles
-const circleRadius = 15 * scalingFactor;
-const circleX = 30 * scalingFactor;
-const orderedCircleColorsAndLabels = [
-  ['--component-started-color', 'Component started'],
-  ['--component-finished-color', 'Component finished'],
-  ['--component-done-color', 'Component done'],
-  ['--statement-passed-color', 'Statement passed'],
-  ['--statement-failed-color', 'Statement failed'],
-  ['--statement-entry-color', 'Loop / Condition entry']
-];
-//// boxes
-const rectX = 17 * scalingFactor;
-const rectWidth = 27 * scalingFactor;
-const rectBorderRadius = 6 * scalingFactor;
-const orderedBoxColorsAndLabels = [
-  ['--task-box-color', 'Task'],
-  ['--service-box-color', 'Service'],
-  ['--condition-box-color', 'Condition'],
-  ['--loop-box-color', 'Loop'],
-  ['--parallel-box-color', 'Parallel']
-];
-//// descriptionText
-const textX = 80 * scalingFactor;
-//// entries
-const lineWidth = 2 * scalingFactor;
-const lineHeight = 40 * scalingFactor;
-const objectsHeightDifferenceCircleAndBox = -10 * scalingFactor; // the boxes are bigger, so make the gap between the circle and box smaller
-let objectsLineStartY = 30 * scalingFactor;
-const fontSize = 27 * scalingFactor;
-const textHeightDifferenceCircleAndBox = 4 * scalingFactor; // same as for the objects
-let textLineStartY = 39 * scalingFactor;
+  // style properties for the legend that are found to fit well
+  //// circles
+  const circleRadius = 15 * scalingFactor;
+  const circleX = 30 * scalingFactor;
+  const orderedCircleColorsAndLabels = [
+    ['--component-started-color', 'Component started'],
+    ['--component-finished-color', 'Component finished'],
+    ['--component-done-color', 'Component done'],
+    ['--statement-passed-color', 'Statement passed'],
+    ['--statement-failed-color', 'Statement failed'],
+    ['--statement-entry-color', 'Loop / Condition entry']
+  ];
+  //// boxes
+  const rectX = 17 * scalingFactor;
+  const rectWidth = 27 * scalingFactor;
+  const rectBorderRadius = 6 * scalingFactor;
+  const orderedBoxColorsAndLabels = [
+    ['--task-box-color', 'Task'],
+    ['--service-box-color', 'Service'],
+    ['--condition-box-color', 'Condition'],
+    ['--loop-box-color', 'Loop'],
+    ['--parallel-box-color', 'Parallel']
+  ];
+  //// descriptionText
+  const textX = 80 * scalingFactor;
+  //// entries
+  const lineWidth = 2 * scalingFactor;
+  const lineHeight = 40 * scalingFactor;
+  const objectsHeightDifferenceCircleAndBox = -10 * scalingFactor; // the boxes are bigger, so make the gap between the circle and box smaller
+  let objectsLineStartY = 30 * scalingFactor;
+  const fontSize = 27 * scalingFactor;
+  const textHeightDifferenceCircleAndBox = 4 * scalingFactor; // same as for the objects
+  let textLineStartY = 39 * scalingFactor;
 
-// draw the legend symbols as coloured circles and rectangles
+  // draw the legend symbols as coloured circles and rectangles
 
-// draw the circles
-for (const [circleColor, circleLabel] of orderedCircleColorsAndLabels) {
-  // draw the colored circle
-  ctx.beginPath();
-  ctx.arc(circleX, objectsLineStartY, circleRadius, 0, 2 * Math.PI);
-  ctx.fillStyle = getComputedStyle(document.documentElement).getPropertyValue(
-    circleColor
-  );
-  ctx.fill();
-  ctx.lineWidth = lineWidth;
-  ctx.stroke();
+  // draw the circles
+  for (const [circleColor, circleLabel] of orderedCircleColorsAndLabels) {
+    // draw the colored circle
+    ctx.beginPath();
+    ctx.arc(circleX, objectsLineStartY, circleRadius, 0, 2 * Math.PI);
+    ctx.fillStyle = getComputedStyle(document.documentElement).getPropertyValue(
+      circleColor
+    );
+    ctx.fill();
+    ctx.lineWidth = lineWidth;
+    ctx.stroke();
 
-  // draw the label
-  ctx.font = String(fontSize) + 'px arial';
-  ctx.fillStyle = '#000000';
-  ctx.fillText(circleLabel, textX, textLineStartY);
+    // draw the label
+    ctx.font = String(fontSize) + 'px arial';
+    ctx.fillStyle = '#000000';
+    ctx.fillText(circleLabel, textX, textLineStartY);
 
-  // set the start height for the next line
-  objectsLineStartY += lineHeight;
-  textLineStartY += lineHeight;
-}
+    // set the start height for the next line
+    objectsLineStartY += lineHeight;
+    textLineStartY += lineHeight;
+  }
 
-// adjust the start height for the next line when switching from circle to box
-objectsLineStartY += objectsHeightDifferenceCircleAndBox;
-textLineStartY += textHeightDifferenceCircleAndBox;
+  // adjust the start height for the next line when switching from circle to box
+  objectsLineStartY += objectsHeightDifferenceCircleAndBox;
+  textLineStartY += textHeightDifferenceCircleAndBox;
 
-// draw the boxes
-for (const [boxColor, boxLabel] of orderedBoxColorsAndLabels) {
-  ctx.beginPath();
-  drawRoundedRect(
-    ctx,
-    rectX,
-    objectsLineStartY,
-    rectWidth,
-    rectWidth,
-    rectBorderRadius
-  );
-  ctx.fillStyle = getComputedStyle(document.documentElement).getPropertyValue(
-    boxColor
-  );
-  ctx.fill();
-  ctx.lineWidth = lineWidth;
-  ctx.stroke();
+  // draw the boxes
+  for (const [boxColor, boxLabel] of orderedBoxColorsAndLabels) {
+    ctx.beginPath();
+    drawRoundedRect(
+      ctx,
+      rectX,
+      objectsLineStartY,
+      rectWidth,
+      rectWidth,
+      rectBorderRadius
+    );
+    ctx.fillStyle = getComputedStyle(document.documentElement).getPropertyValue(
+      boxColor
+    );
+    ctx.fill();
+    ctx.lineWidth = lineWidth;
+    ctx.stroke();
 
-  // draw the label
-  ctx.font = String(fontSize) + 'px arial';
-  ctx.fillStyle = '#000000';
-  ctx.fillText(boxLabel, textX, textLineStartY);
+    // draw the label
+    ctx.font = String(fontSize) + 'px arial';
+    ctx.fillStyle = '#000000';
+    ctx.fillText(boxLabel, textX, textLineStartY);
 
-  // set the start height for the next line
-  objectsLineStartY += lineHeight;
-  textLineStartY += lineHeight;
+    // set the start height for the next line
+    objectsLineStartY += lineHeight;
+    textLineStartY += lineHeight;
+  }
 }
